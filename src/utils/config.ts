bded856922ac23d8ad3c1a96e9f979c931080d81
bigchiano <bigchiano@protonmail.com>
2024-01-22 13:49:56 +0200

    add mint and program addresses

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const config = {
-  usdcMintPubKey: new PublicKey(""),
-  escrowProgramId: new PublicKey(""),
+  usdcMintPubKey: new PublicKey("7ecFQrxz4oiFRKzy3yfVLekMzZPK1j1SA2Fn8kuoe7Q5"),
+  escrowProgramId: new PublicKey("Bm5SDVKRqHuQxUSjVGA4cYFWTVRYS14HYy7M4GxtZkht"),
 };
 
