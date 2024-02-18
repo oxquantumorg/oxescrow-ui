07d05d2c4fcac7c26fca739a7e5c345169c4e5ed
bigchiano <bigchiano@protonmail.com>
2024-02-18 03:53:28 +0200

    change escrow program id

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,6 +4,6 @@ export const config = {
   localhost: undefined,
   usdcMintPubKey: new PublicKey("DK6BeYfBvcb7epFnDiEXvqZKVbiPjiV692h7oWoB5P5L"),
   escrowProgramId: new PublicKey(
-    "Ah4p28tRTEkv4Dnt7UwN1xRYNRgb6ofRLbqqa5zrPPLC"
+    "EQ241DM743htRoAQ1WS8S8QDL1g4dKkndCLuUQ552zgS"
   ),
 };
