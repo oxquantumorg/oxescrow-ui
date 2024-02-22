0ff9aa36c56428e3e567a93acc8d7e8a04617687
bigchiano <bigchiano@protonmail.com>
2024-02-22 16:41:56 +0200

    remove escrow

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo mx-auto" alt="logo" />
         <h3 className="text-white text-center">
-          Oxescrow Escrow Systems
+          Oxescrow Systems
         </h3>
         <h3 className="text-white text-center">
           {wallet.connected && wallet.publicKey && (
